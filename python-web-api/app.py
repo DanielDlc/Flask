e8da655845ce7b68e4fd731aa5b1633ada3be00d
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-15 13:01:30 +0100

    erro handler 404

--- python-web-api/app.py
+++ python-web-api/app.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["APP_NAME"] = "My Blog"
+
+
+@app.errorhandler(404)
+def not_found_page(error):
+    return f"Not Found on {app.config['APP_NAME']}"
+
+
 
 @app.route("/")
 def hello():
