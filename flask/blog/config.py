0bb3689d5969896c4b435c32825ca5f6e39572e1
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-16 15:19:52 +0100

    exensions e root path

--- flask/blog/config.py
+++ flask/blog/config.py
@@ -4,4 +4,4 @@ from dynaconf import FlaskDynaconf
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 def configure(app):
-    FlaskDynaconf(app)
+    FlaskDynaconf(app, extensions_list="EXTENSIONS", root_path=HERE)
