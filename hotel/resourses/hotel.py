a49f33e005e6f5d87f467dce27763f86e03b0109
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-27 11:13:53 +0100

    arquivo hotel

---
+++ hotel/resourses/hotel.py
@@ -0,0 +1,32 @@
+from flask_restful import Resource
+
+
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'alpha hotel',
+        'estrelas': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Rio de Janeiro'
+    },
+
+    {
+        'hotel_id': 'bravo',
+        'nome': 'bravo hotel',
+        'estrelas': 4.4,
+        'diaria': 380.90,
+        'cidade': 'Santa Catarina'
+    },
+
+    {
+        'hotel_id': 'charlie',
+        'nome': 'charlie hotel',
+        'estrelas': 3.9,
+        'diaria': 320.20,
+        'cidade': 'São Paulo'
+    }
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis}
