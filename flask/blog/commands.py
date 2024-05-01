53e63a9f435cf0ec910b52658aa446404accf8c0
DanielDlc <dlc.engcomputacao@gmail.com>
2024-05-01 09:05:13 +0100

    commands

--- flask/blog/commands.py
+++ flask/blog/commands.py
@@ -7,11 +7,9 @@ from blog.posts import (
     update_post_by_slug,
 )
 
-
 @click.group()
 def post():
-    """MAnage blog post"""
-
+    """Manage blog post"""
 
 @post.command()
 @click.option("--title")
@@ -20,6 +18,33 @@ def new(title, content):
     new = new_post(title, content)
     click.echo(f"New post created {new}")
 
+@post.command("list")
+def _list():
+    """ Lists all posts"""
+    for post in get_all_posts():
+        click.echo(post)
+        click.echo("-" * 30)
+
+@post.command()
+@click.argument("slug")
+def get(slug):
+    """Get post by slug"""
+    post = get_post_by_slug(slug)
+    click.echo(post or "post not found")
+
+@post.command()
+@click.argument("slug")
+@click.option("--content", default=None, type=str)
+@click.option("--published", default=None, type=bool)
+def update(slug, content, published):
+    """Update post by slug"""
+    data = {}
+    if content is not None:
+        data["content"] = content
+    if published is not None:
+        data["published"] = published
+    update_post_by_slug(slug, data)
+    click.echo("Post Updated")
 
 def configure(app):
     app.cli.add_command(post)
