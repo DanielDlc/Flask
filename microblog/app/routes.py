01168a3a3a6691bca5c8754b4e8587ef1c38b326
DanielDlc <dlc.engcomputacao@gmail.com>
2024-05-01 09:04:59 +0100

    routes

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+from app import db
+from app.models import User
 from app import app
 from app.forms import LoginForm
+import sqlalchemy as sa
 
 @app.route('/')
 @app.route('/index')
@@ -20,8 +24,13 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
         flash('Login requested for use {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
