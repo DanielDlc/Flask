abbdd33291877443d89cf02e04d4ea60b5266635
DanielDlc <dlc.engcomputacao@gmail.com>
2024-05-01 12:01:11 +0100

    logout

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app import db
 from app.models import User
 from app import app
@@ -29,9 +29,15 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(
-            sa.select(User).where(User.username == form.username.data)
-        )
-        flash('Login requested for use {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
